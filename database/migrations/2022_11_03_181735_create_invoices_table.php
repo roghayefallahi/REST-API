7a57f5ee0ed761872a47326922a974e4b8348002
rofa <roghaye.fallahi75@gmail.com>
2022-11-05 13:59:59 +0330

    rest-api

---
+++ database/migrations/2022_11_03_181735_create_invoices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->string('amount');
+            $table->string('status'); // billed, paid, void
+            $table->dateTime('billed_dated');
+            $table->dateTime('paid_dated')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
