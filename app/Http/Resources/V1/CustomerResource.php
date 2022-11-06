550cf521aeb01d88bd55ccaebad8409c2c49d9bf
rofa <roghaye.fallahi75@gmail.com>
2022-11-06 21:55:42 +0330

    create resource and collection

--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -14,6 +14,16 @@ class CustomerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+       // return parent::toArray($request);
+       return [
+        'id' => $this->id,
+        'name' => $this->name,
+        'type' => $this->type,
+        'email' => $this->email,
+        'address' => $this->address,
+        'city' => $this->city,
+        'state' => $this->state,
+        'postalCode' => $this->postal_code,
+       ];
     }
 }
